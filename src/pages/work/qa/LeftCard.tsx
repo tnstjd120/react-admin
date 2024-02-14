ffb3d3ef63ac62636a291d1171fd72fb63c508c7
tnstjd120 <tnstjd120@gmail.com>
2024-02-15 00:49:15 +0900

    fix: LeftCard 컴포넌트 데이터 형태에 맞춰 제작중

--- src/pages/work/qa/LeftCard.tsx
+++ src/pages/work/qa/LeftCard.tsx
@@ -1,4 +1,5 @@
 import BlankCard from "@/components/common/BlankCard";
+import Scrollbar from "@/components/common/Scrollbar";
 import theme from "@/utils/theme";
 import {
   Accordion,
@@ -6,30 +7,28 @@ import {
   AccordionProps,
   AccordionSummary,
   Box,
-  Button,
-  Card,
   Chip,
-  Collapse,
   ImageList,
   ImageListItem,
-  List,
-  ListItem,
+  ImageListItemBar,
+  Typography,
   styled,
 } from "@mui/material";
-import { IconChevronDown, IconChevronUp } from "@tabler/icons-react";
-import { ReactNode, SyntheticEvent, useState } from "react";
-
-type Props = {
-  // children: ReactNode;
-};
+import { blue } from "@mui/material/colors";
+import { IconChevronDown } from "@tabler/icons-react";
+import { SyntheticEvent, useState } from "react";
 
 const CustomAccordion = styled(Accordion)<AccordionProps>(({ theme }) => ({
-  "& .Mui-expanded": {
-    backgroundColor: theme.palette.primary.main,
-
-    "& .MuiCollapse-root": {
-      backgroundColor: `${theme.palette.primary.main} !important`,
-    },
+  "& > .Mui-expanded": {
+    borderTop: `1px solid ${theme.palette.primary.main}`,
+    borderLeft: `1px solid ${theme.palette.primary.main}`,
+    borderRight: `1px solid ${theme.palette.primary.main}`,
+    borderRadius: "8px 8px 0 0",
+  },
+  "& .MuiAccordionDetails-root": {
+    border: `1px solid ${theme.palette.primary.main}`,
+    borderTop: `1px solid #ddd`,
+    borderRadius: "0 0 8px 8px",
   },
 }));
 
@@ -37,51 +36,101 @@ const LeftCard = () => {
   const [expanded, setExpanded] = useState<string | false>(false);
   const handleChangeReceipt =
     (value: string) => (event: SyntheticEvent, isExpanded: boolean) => {
-      console.log("value => ", value);
-      console.log("isExpanded => ", isExpanded);
       setExpanded(isExpanded ? value : false);
     };
+
+  // const [currentImage, setCurrentImage] = useState(null)
+  // const handleChangeCurrentImage = (imageUid: number) => {
+  //   setCurrentImage()
+  // }
   return (
     <LeftCardContainer>
-      {Array(20)
-        .fill(0)
-        .map((_, i) => (
-          <CustomAccordion
-            key={i}
-            expanded={expanded === `receipt${i}`}
-            onChange={handleChangeReceipt(`receipt${i}`)}
-            variant="outlined"
-          >
-            <AccordionSummary expandIcon={<IconChevronDown />}>
-              <Box display="flex" alignItems="center" gap={1}>
-                <Chip label={i} color="primary" size="small" />
+      <Scrollbar sx={{ height: "100%" }}>
+        {Array(20)
+          .fill(0)
+          .map((_, i) => (
+            <CustomAccordion
+              key={i}
+              expanded={expanded === `receipt${i}`}
+              onChange={handleChangeReceipt(`receipt${i}`)}
+            >
+              <AccordionSummary expandIcon={<IconChevronDown />}>
+                <Box display="flex" alignItems="center" gap={1}>
+                  <Typography variant="subtitle2">9024589781{i}</Typography>
+                  <Typography variant="subtitle2" fontWeight="bold">
+                    [{2 * i}]
+                  </Typography>
+                </Box>
+              </AccordionSummary>
 
-                <span>9024589781{i}</span>
-              </Box>
-            </AccordionSummary>
+              <AccordionDetails>
+                <Scrollbar sx={{ height: "50vh" }}>
+                  <ImageList
+                    sx={{ width: "100%" }}
+                    cols={1}
+                    rowHeight={200}
+                    gap={20}
+                  >
+                    {Array(10)
+                      .fill(0)
+                      .map((_, j) => (
+                        <ImageListItem
+                          key={j}
+                          sx={{
+                            width: "100%",
+                            borderRadius: "8px",
+                            overflow: "hidden",
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            backgroundColor:
+                              j === 2 ? theme.palette.primary.main : "white",
+                            p: 1,
+                          }}
+                        >
+                          {![6, 7, 10].includes(i) && (
+                            <Chip
+                              label={j}
+                              color="primary"
+                              sx={{
+                                position: "absolute",
+                                left: "5px",
+                                top: "5px",
+                              }}
+                            />
+                          )}
 
-            <AccordionDetails>
-              <ImageList
-                sx={{ width: "100%", maxHeight: "412px" }}
-                cols={1}
-                rowHeight={80}
-              >
-                {Array(10)
-                  .fill(0)
-                  .map((_, j) => (
-                    <ImageListItem key={j} sx={{ width: "100%" }}>
-                      <img
-                        width="100%"
-                        src="https://upload.wikimedia.org/wikipedia/commons/c/c3/LibreOffice_Writer_6.3.png"
-                        alt={`이미지 ${j}`}
-                        loading="lazy"
-                      ></img>
-                    </ImageListItem>
-                  ))}
-              </ImageList>
-            </AccordionDetails>
-          </CustomAccordion>
-        ))}
+                          <img
+                            width="100%"
+                            src="https://upload.wikimedia.org/wikipedia/commons/c/c3/LibreOffice_Writer_6.3.png"
+                            alt={`이미지 ${j}`}
+                            loading="lazy"
+                          />
+
+                          <ImageListItemBar
+                            title={
+                              <Typography
+                                variant="h6"
+                                color={j % 2 === 0 ? "primary" : "grey"}
+                                fontWeight="bold"
+                              >
+                                {j % 2 === 0 ? "완료" : "미완료"}
+                              </Typography>
+                            }
+                            subtitle={`20240215245d000${j}`}
+                            position="bottom"
+                            sx={{
+                              backgroundColor: "rgba(0,0,0,0.8)",
+                            }}
+                          />
+                        </ImageListItem>
+                      ))}
+                  </ImageList>
+                </Scrollbar>
+              </AccordionDetails>
+            </CustomAccordion>
+          ))}
+      </Scrollbar>
 
       {/* <List>
         <ListItem sx={{ display: "flex", flexDirection: "column" }}>
@@ -118,7 +167,7 @@ const LeftCard = () => {
 export default LeftCard;
 
 const LeftCardContainer = styled(BlankCard)`
-  min-width: 200px;
-  max-width: 200px;
-  max-height: 100%;
+  min-width: 250px;
+  max-width: 250px;
+  height: 100%;
 `;
