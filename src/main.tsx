4c1001ba62f2a3534b2b39695d213a625101daa3
tnstjd120 <tnstjd120@gmail.com>
2024-02-15 00:50:06 +0900

    fix: mocking 제외 처리리

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,11 @@ import "./assets/fonts.css";
 import { SnackbarProvider } from "notistack";
 
 const enableMocking = async () => {
-  // return;
-  if (!import.meta.env.DEV) return;
+  return;
+  // if (!import.meta.env.DEV) return;
 
-  const { worker } = await import("./mocks/browser.ts");
-  return worker.start();
+  // const { worker } = await import("./mocks/browser.ts");
+  // return worker.start();
 };
 
 enableMocking().then(() => {
